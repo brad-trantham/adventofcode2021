d6ee0af811f9575d177f3c000d7cf836535fbfd5
Brad Trantham <trantham@gmail.com>
2021-12-02 20:45:27 -0600

    Renamed input file for second half of day 1

--- tests/DepthMeasurerTest.py
+++ tests/DepthMeasurerTest.py
@@ -10,9 +10,8 @@ class DepthMeasurerTest(unittest.TestCase):
         count = depth_measurer.calculate_depth_changes(measurements)
         self.assertEqual(7, count)
 
-
     def test_calculate_window_changes(self):
-        input_file = "/Users/brad/github/advent2021/input2_small.txt"
+        input_file = "/Users/brad/github/advent2021/input1b_small.txt"
         measurements = depth_measurer.read_inputs(input_file)
         count = depth_measurer.calculate_window_changes(measurements)
         self.assertEqual(5, count)
